1c09c0e5dd0b1123dd7d301d5b024fc3e87c28de
lokappakishan <lokappakishan@gmail.com>
2023-01-06 19:06:30 +0100

    adding scss and navbar styles

--- components/Header.js
+++ components/Header.js
@@ -6,10 +6,9 @@ import Image from "next/image";
 import mypics from '../assets/logo.png'
 
 
-
 export default function Header(){
     return(
-    <header>
+    <header style={{textAlign:'center'}} >
         <div className="bar">
             <Link href={'/'}>
                 <Image src={mypics} alt="Image" width="350px" height="300px" />
--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,36 @@
 import Link from "next/link";
 import React from "react";
+import styled from "styled-components";
+
+const Navbar = styled.nav`
+
+    ul{
+        border: 1px solid magenta;
+        display: flex;
+        justify-content: center;
+        
+    }
+
+    li{
+        border: 2px dotted orange;
+        list-style: none;
+        margin: 0 1em;
+    }
+
+    a{
+        color: white;
+        text-decoration: none;
+    }
+
+`;
 
 export default function Nav(){
     return(
-    <nav>
+    <Navbar>
         <ul>
             <li><Link href='/locations' >LOCATIONS</Link></li>    
             <li><Link href='/characters' >CHARACTERS</Link></li>
             <li><Link href='/episodes' >EPISODES</Link></li>
         </ul>
-    </nav>);
+    </Navbar>);
 }
