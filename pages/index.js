ed11ca727217d771b5a64ecbf14984a42444330c
lokappakishan <lokappakishan@gmail.com>
2023-01-07 02:01:42 +0100

    added main section

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,20 @@ import { ApolloProvider, gql } from '@apollo/client'
 import App from '../container/App'
 import client from '../apollo-client'
 import Page from '../components/Page'
-
-const inter = Inter({ subsets: ['latin'] })
+import heroImage from '../assets/rickAndMorty1.jpeg';
 
 export default function Home() {
 
   return (
-    <>home</>
+    <section className={`${styles.container} ${styles.heroSection}`}>
+      <section className={styles.leftSection}>
+        <h1>Rick And Morty</h1>
+        <h3>Experience the Interdimensional Adventures of Rick and Morty</h3>
+        <h3>Join Rick, Morty, and the rest of the Smith family on their wild rides through the multiverse</h3>
+      </section>
+      <section className={styles.rightSection}>
+        <Image src={heroImage} alt='heroRickAndMorty' />
+      </section>
+    </section>
   )
 };
