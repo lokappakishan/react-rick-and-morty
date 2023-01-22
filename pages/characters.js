bd1526f3352d2524c17bb6b0ab977b18e771b4ed
lokappakishan <lokappakishan@gmail.com>
2023-01-23 00:01:28 +0100

    handle error while accessing apis

--- pages/characters.js
+++ pages/characters.js
@@ -26,6 +26,20 @@ const variables = {page:1};
 export default function CharactersPage(){
 
     const {loading,error,data} = useQuery(GET_ALL_CHARACTERS,variables);
+
+    if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!data) {
+    return <div>No data found.</div>;
+  }
+
+
     const  {characters: {info: infoList,results: charactersList}} = data;
 
     console.log(infoList,charactersList);
